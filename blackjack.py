4b864981cf03f84932b27e8a818d39e2ca46d5ac
Pelayo <phuermij@myuax.com>
2021-11-20 13:02:18 +0100

    Codigo de ejemplo

---
+++ blackjack.py
@@ -0,0 +1,38 @@
+from random import choice, sample
+
+cartas = {
+    chr(0x1f0a1): 11,
+    chr(0x1f0a2): 2,
+    chr(0x1f0a3): 3,
+    chr(0x1f0a4): 4,
+    chr(0x1f0a5): 5,
+    chr(0x1f0a6): 6,
+    chr(0x1f0a7): 7,
+    chr(0x1f0a8): 8,
+    chr(0x1f0a9): 9,
+    chr(0x1f0aa): 10,
+    chr(0x1f0ab): 10,
+    chr(0x1f0ad): 10,
+    chr(0x1f0ae): 10,
+}
+
+print("Cartas: {}".format(" ".join(cartas.keys())))
+print("Puntos: {}".format(list(cartas.values())))
+
+for carta, valor in cartas.items():
+    print("la carta {} vale {}".format(carta,valor))
+
+
+for carta in sorted(cartas.keys()):
+    print("la carta {} vale {}".format(carta, cartas[carta]))
+
+lista_cartas = list(cartas)
+
+print("Ha seleccionado: ", end=" ")
+carta = choice(lista_cartas)
+score = cartas[carta]
+print(carta, end=" ")
+carta = choice(lista_cartas)
+score += cartas[carta]
+print(carta, end=" ")
+print("Su puntuación es de: ", score)
